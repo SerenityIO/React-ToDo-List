a5e69c1a7289e64a7894f5f771ac59c9bb393f78
Nazar001 <39243794+Nazar001@users.noreply.github.com>
2019-02-13 14:58:12 +0200

    Ready project

--- main.js
+++ main.js
@@ -2,10 +2,37 @@ function handleSubmitForm(event) {
     event.preventDefault();
     
     var li = document.createElement('li');
-
+    
     li.innerText = event.target[0].value;
+    
+    li.setAttribute("onclick", "toggle(this);");
+
+    li.className=" ";
+
+    var div = document.createElement('div');
+
+    div.setAttribute("onclick", "remove(this);");
+
+    div.className="button";
+
+    li.appendChild(div);
 
     var list = document.getElementById('list');
 
-    list.innerHTML = li;
+    list.appendChild(li);
+
+}
+
+function toggle(el) {
+    el.className = (el.className == ' ') ? 'active' : ' ';
+
+}
+function remove(el){
+
+    var per = el.parentNode;
+
+    var list = document.getElementById('list');
+
+    list.removeChild(per);
+
 }
