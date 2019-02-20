0112e8c40b1d85a1526286f44b3697de5da5fe3c
Nazar001 <39243794+Nazar001@users.noreply.github.com>
2019-02-21 00:02:45 +0200

    Copy is ready

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ var array = [
         name: 'Test 3',
         done: false
     }
+    ,
+    {
+        name: 'Test 4',
+        done: false
+    }
 ];
 
 window.onload = function() {
@@ -25,72 +30,72 @@ window.onload = function() {
 
 window.onunload = () => {
     window.localStorage.setItem('array', JSON.stringify(array));
+    render();
 };
 
 function render() {
     list.innerHTML = '';
-    for (let i = 0; i < array.length; i++) {     
-        var li = document.createElement('li');
-        var check = document.createElement('input');
-        var div = document.createElement('div');
-        var but = document.createElement('div');
-        var done = document.createElement('button');
-        var edit = document.createElement('button');
-        var copy = document.createElement('button');
+    for (let i = 0; i < array.length; i++) {
+        if(array[i]) {
+            var li = document.createElement('li');
+            var check = document.createElement('input');
+            var div = document.createElement('div');
+            var but = document.createElement('div');
+            var done = document.createElement('button');
+            var edit = document.createElement('button');
+            var copy = document.createElement('button');
+            
+            done.type = 'submit';
+            done.addEventListener('click', function() {
+                audit(i);
+            });
+            if(array[i].done) {
+                done.innerText = 'Undone';
+                done.className = 'Undone';
+                li.style.backgroundColor = "rgb(193, 255, 193)";
+                li.style.borderTop = "1px solid rgb(0, 255, 13)";
+                li.style.borderBottom = "1px solid rgb(0, 255, 13)";
+                li.style.height = "30px";
+            } else {
+                done.innerText = 'Done';
+                done.className = 'Done';
+                li.style.backgroundColor = "";
+                li.style.border = "";
+                li.style.height = "";
+            }
+            edit.type = 'submit';
+            edit.innerText = 'Edit';
+            edit.className = 'Edit';
+            edit.addEventListener('click', function(){
+                editText(i);
+            });
+            copy.type = 'submit';
+            copy.innerText = 'Copy';
+            copy.className = 'Copy';
+            copy.addEventListener('click', function(){
+                copyrate(i);
+            });
+            but.className = "DoneEditCopy";
+            but.appendChild(done);
+            but.appendChild(edit);
+            but.appendChild(copy);
+            check.type = "checkbox";
+            check.className = "check"; 
+            check.addEventListener('click', function(){
+                checkLi(i);
+            });
+            li.innerText = array[i].name;
+            li.id = i;
+            div.addEventListener('click', function(){
+                remove(i);
+            });
         
-        done.type = 'submit';
-        done.addEventListener('click', function() {
-            audit(i);
-        });
-        if(array[i].done) {
-            done.innerText = 'Undone';
-            done.className = 'Undone';
-            li.style.backgroundColor = "rgb(193, 255, 193)";
-            li.style.borderTop = "1px solid rgb(0, 255, 13)";
-            li.style.borderBottom = "1px solid rgb(0, 255, 13)";
-            li.style.height = "30px";
-        } else {
-            done.innerText = 'Done';
-            done.className = 'Done';
-            li.style.backgroundColor = "";
-            li.style.border = "";
-            li.style.height = "";
-        }
-        
-        edit.type = 'submit';
-        edit.innerText = 'Edit';
-        edit.className = 'Edit';
-        edit.addEventListener('click', function(){
-            editText(i);
-        });
-
-        copy.type = 'submit';
-        copy.innerText = 'Copy';
-        copy.className = 'Copy';
-
-        but.className = "DoneEditCopy";
-        but.appendChild(done);
-        but.appendChild(edit);
-        but.appendChild(copy);
-
-        check.type = "checkbox";
-        check.className = "check"; 
-        check.addEventListener('click', function(){
-
-        });
-
-        li.innerText = array[i].name;
-        li.id = i;
-        
-        div.addEventListener('click', function(){
-            remove(i);
-        });
-        
-        div.className = "button";
-        li.appendChild(check);
-        li.appendChild(div);
-        li.appendChild(but);
-        list.appendChild(li);   
+            div.className = "button";
+            li.appendChild(check);
+            li.appendChild(div);
+            li.appendChild(but);
+            list.appendChild(li); 
+        }  
     }
 }
 
@@ -98,7 +103,12 @@ function handleSubmitForm(event) {
     event.preventDefault();
     
     var li = document.createElement('li');
+    var check = document.createElement('input');
     
+    check.type = "checkbox";
+    check.className = "check"; 
+    li.appendChild(check);
+
     li.innerText = event.target[0].value;
     array.push({
         name: event.target[0].value,
@@ -108,35 +118,52 @@ function handleSubmitForm(event) {
     render();
 }
 
-function checkAll(){
-    var check = document.getElementsByClassName('check');
-    var all = document.getElementById('all');
-    if(all.checked){
-        for (var i = 0; i < check.length; i++){
-            var per = check[i].parentNode;
-            
-            check[i].checked = true;
-            per.className = 'red';
-        }
-    } else {
-        for (var i = 0; i < check.length; i++){
-            var per = check[i].parentNode;
-            
-            check[i].checked = false;
-            per.className = '';
-        }
-    }
+function copyrate(id){
+    array.push({
+        name: array[id].name,
+        done: array[id].done
+    });
+    render();
 }
 
-function removeCheck(event){
-    var redLi = document.getElementsByClassName('red');
-    var all = document.getElementById('all');
-
-    all.checked = false;
-    for (var i = redLi.length - 1; i >= 0; i--) {
-        list.removeChild(redLi[i]);
-    }
+function checkLi(id){
+    // var del = document.getElementsByClassName('check');
+    var li = document.getElementsByTagName('li');
+    debugger;
+    // li[id].className = (this.checked == true) ? 'red' : '';
+    li[id].className = "red";
+    debugger;
 }
+
+// function checkAll(){
+//     var check = document.getElementsByClassName('check');
+//     var all = document.getElementById('all');
+//     if(all.checked){
+//         for (var i = 0; i < check.length; i++){
+//             var per = check[i].parentNode;
+            
+//             check[i].checked = true;
+//             per.className = 'red';
+//         }
+//     } else {
+//         for (var i = 0; i < check.length; i++){
+//             var per = check[i].parentNode;
+            
+//             check[i].checked = false;
+//             per.className = '';
+//         }
+//     }
+// }
+
+// function removeCheck(event){
+//     var redLi = document.getElementsByClassName('red');
+//     var all = document.getElementById('all');
+
+//     all.checked = false;
+//     for (var i = redLi.length - 1; i >= 0; i--) {
+//         list.removeChild(redLi[i]);
+//     }
+// }
 
 function remove(id){
     array = array.filter(function(value, index){
