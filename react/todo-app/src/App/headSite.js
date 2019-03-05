5a7fad6454296b71db674cc14ec77188b4dde439
Nazar001 <39243794+Nazar001@users.noreply.github.com>
2019-03-05 21:11:09 +0200

    1

--- react/todo-app/src/App/headSite.js
+++ react/todo-app/src/App/headSite.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import '../css/headSite.css'
+import React from 'react';
+import '../css/headSite.css';
 
-function headSite() {
-    return (
-        <header>
-            <h3>My ToDo-List</h3>
-            <form>
-                <input placeholder = "Tide" required  required type = "text"/>
-                <button type = 'submit'>Add</button>
-            </form>
-            <menu>
-                <input type="checkbox" name="all" id="all" onclick="checkAll(event);"/>
-                <button type="submit" onclick="removeCheck(event);" id="deleteAll">Remove</button>
-                <button type='submit' onclick="WindowReplace();" id ='load'>Load</button>
-            </menu>
-        </header>
-    )
-}
+const HeadSite = () => (
+    <header>
+        <h3>My ToDo-List</h3>
+        <form>
+            <input placeholder="Tide" required required type="text" />
+            <button type='submit'>Add</button>
+        </form>
+        <menu>
+            <input type="checkbox" name="all" id="all" onclick="checkAll(event);" />
+            <button type="submit" onclick={removeCheck} id="deleteAll">Remove</button>
+            <button type='submit' onclick="WindowReplace();" id='load'>Load</button>
+        </menu>
+    </header>
+);
 
-headSite();
-export default headSite
+export default HeadSite;
